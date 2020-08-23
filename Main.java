c677341c77320ba0ef13358564944746ac886fed
EunJiGil <55980214+EunJiGil@users.noreply.github.com>
2020-08-23 19:50:32 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+package rhythmgame4;
+
+public class Main {
+
+	public static final int SCREEN_WIDTH = 1280;
+	public static final int SCREEN_HEIGHT = 720;
+	public static final int NOTE_SPEED = 3;
+	public static final int SLEEP_TIME = 10;
+	public static final int REACH_TIME = 2; //판정바에 도달할때까지
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		new Rhythmgame();
+		
+	}
+
+}
